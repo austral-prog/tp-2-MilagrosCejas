422530229218372e5f3c32700541ce5f381b3879
MilagrosCejas <mcejas@mail.austral.edu.ar>
2025-03-28 20:13:45 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,20 @@
+import math
+
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()
+    print("Vuelto")
+    print()
+    fractional, integer = math.modf(money - expense)
+    print("Pesos:")
+    print(round(integer))
+    print("Centavos:")
+    print(round(fractional * 100))
+
+change()
